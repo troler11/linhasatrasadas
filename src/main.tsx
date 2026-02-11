c911e3b67cb39f707844066def44226a99312e3c
troler11 <lucasbubu22@gmail.com>
2026-02-11 09:57:50 -0300

    Update App import and comment out CSS import
    Removed the .tsx extension from the App import and commented out the CSS import.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App' // Removido o .tsx daqui
+// import './index.css' // Comente se não tiver o arquivo css ainda
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
